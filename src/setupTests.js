858ac49b9ecd3aa8507f462c4575a17144692395
Al McKinlay <github@10people.co.uk>
2020-07-06 18:51:53 +0100

    Add electron require to tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+window.require = (module) => {{
+    if (module !== "electron") {
+        return require(module);
+    } else {
+        return {
+            remote: {
+                require: require
+            }
+        }
+    }
+}};
