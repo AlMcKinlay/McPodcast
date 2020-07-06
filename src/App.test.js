d4b67940d190fd76a437197febb963df9fc52c83
Al McKinlay <github@10people.co.uk>
2020-07-06 18:52:29 +0100

    Fix app.js test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Let's add some chapters/i);
+  const linkElement = getByText(/Drop an mp3 file, or click here to select one./i);
   expect(linkElement).toBeInTheDocument();
 });
