9dc65d66e51d07fecf90c1ba5466d6fa752c0738
Al McKinlay <github@10people.co.uk>
2021-09-02 13:12:40 +0100

    Fix issue with new chapter having an existing chapter id

--- src/fileEditor/ChapterPanel.js
+++ src/fileEditor/ChapterPanel.js
@@ -20,26 +20,28 @@ export default function ChapterPanel({ chapters, setChapters }) {
 	const [editingChapter, setEditingChapter] = useState(null);
 	const addChapter = () => {
 		const newChapters = chapters ? [...chapters] : [];
+		const startTimeMs = chapters.length > 0 ? chapters[chapters.length - 1].endTimeMs : 0;
 		newChapters.push({
-			elementID: `Ch${newChapters.length}`,
-			startTimeMs: 0,
-			endTimeMs: 0,
+			elementID: `Ch${newChapters.length + 1}`,
+			startTimeMs,
+			endTimeMs: startTimeMs + 1000,
 			tags: { title: "" },
+			newlyCreated: true,
 		});
 		setChapters(newChapters);
 		setEditingChapter(newChapters[newChapters.length - 1].elementID);
 	};
 	const updateChapter = (index) => (update) => {
-		console.log(update);
 		const newChapters = [...chapters];
 		newChapters[index] = update;
+		delete newChapters[index].newlyCreated;
 		setChapters(newChapters);
 		setEditingChapter(null);
 	};
 	const cancelEditing = (index) => () => {
 		setEditingChapter(null);
 		const chapter = chapters[index];
-		if (chapter.tags.title.trim() === "" && chapter.startTimeMs === 0 && chapter.endTimeMs === 0) {
+		if (chapter.newlyCreated) {
 			const newChapters = [...chapters];
 			newChapters.splice(index);
 			setChapters(newChapters);
