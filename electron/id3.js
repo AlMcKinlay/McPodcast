66d4e6eb69f47da6a2a856ef5f0159cbbeddd700
Al McKinlay <github@10people.co.uk>
2022-04-04 20:51:47 +0100

    Fix bug with sometimes not having a time for the mp3

--- electron/id3.js
+++ electron/id3.js
@@ -3,19 +3,25 @@ const msToTime = require("../src/shared/utils").msToTime;
 const timeToMS = require("../src/shared/utils").timeToMS;
 const mp3Duration = require("mp3-duration");
 
+const timeout = (prom, time) => Promise.race([prom, new Promise((_r, rej) => setTimeout(rej, time))]);
+
 exports.getTags = async (file) => {
-	const tags = NodeID3.read(file);
+	const tags = NodeID3.read(file) || {};
 	delete tags.raw;
 
-	if (tags.image.imageBuffer) {
+	if (tags.image?.imageBuffer) {
 		tags.image.base64 = Buffer.from(tags.image.imageBuffer).toString("base64");
 	}
 
 	if (tags.length) {
 		tags.length = msToTime(tags.length);
 	} else {
-		const seconds = await mp3Duration(file);
-		tags.length = msToTime(seconds * 1000);
+		try {
+			const seconds = await timeout(mp3Duration(file), 5000);
+			tags.length = msToTime(seconds * 1000);
+		} catch (e) {
+			console.log("No time");
+		}
 	}
 
 	return tags;
